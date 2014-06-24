d88fcc05ed11729da170c5d4da9192a9edf1913e
rka <rka@hotelmediaservice.com>
2014-06-24 15:30:31 +0200

    some minor improvements

--- js/main.js
+++ js/main.js
@@ -115,13 +115,11 @@ var SEPACodeScanner = {
 
             iban = document.getElementById(f_iban);
             iban.value = lines[6];
-            iban.blur();
-            iban.onchange();
+            // iban.blur();
             
             bic = document.getElementById(f_bic);
             bic.value = lines[4];
             bic.blur();
-            bic.onchange();
             
             document.getElementById(f_name).value = lines[5];
             document.getElementById(f_amount).value = lines[7];
